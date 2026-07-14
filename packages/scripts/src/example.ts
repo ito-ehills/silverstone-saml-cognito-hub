8680da727b6e0790bf259a115054b8b07ee154eb
ito-ehills <ito@ehills.co.jp>
2026-07-14 12:11:22 +0900

    My first commit

--- packages/scripts/src/example.ts
+++ packages/scripts/src/example.ts
@@ -1,4 +1,4 @@
 import { Resource } from "sst";
-import { Example } from "@monorepo-template/core/example";
+import { Example } from "@silverstone-saml-cognito-hub/core/example";
 
 console.log(`${Example.hello()} Linked to ${Resource.MyBucket.name}.`);
--- sst.config.ts
+++ sst.config.ts
@@ -3,12 +3,17 @@
 export default $config({
   app(input) {
     return {
-      name: "monorepo-template",
+      name: "silverstone-saml-cognito-hub",
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
     };
   },
+  async run() {
+    await import("./infra/storage");
+    await import("./infra/api");
+  },
+/*
   async run() {
     const storage = await import("./infra/storage");
     await import("./infra/api");
@@ -17,4 +22,5 @@ export default $config({
       MyBucket: storage.bucket.name,
     };
   },
+*/
 });
