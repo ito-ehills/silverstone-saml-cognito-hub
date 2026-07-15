72de1d4ea134041b98951d15d5bbcdc6f859cd41
ito-ehills <ito@ehills.co.jp>
2026-07-15 11:08:27 +0900

    Setting up React App for our frontend

---
+++ packages/frontend/src/containers/Home.tsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Silverstone</h1>
+        <p className="text-muted">A simple Silverstone SAML app</p>
+      </div>
+    </div>
+  );
+}
